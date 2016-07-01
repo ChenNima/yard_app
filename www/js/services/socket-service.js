d7020275422e680f4106dae3d4384e55c6f93dad
Yifei Chen <yifei.chen@careerbuilder.com>
2016-07-01 16:58:45 +0800

    initial commit

---
+++ www/js/services/socket-service.js
@@ -0,0 +1,38 @@
+/**
+ * Created by CYF on 16/6/28.
+ */
+angular.module('socketService', [])
+    .factory('socketService',[
+        '$rootScope',
+        function ($rootScope) {
+    var socket = io.connect('http://115.28.109.109/');
+    return {
+        reconnect : function () {
+            if(socket.connected){
+                socket.disconnect();
+            }
+            socket = io.connect();
+        },
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        },
+        disconnect:function(){
+            socket.disconnect();
+        }
+    };
+}]);
