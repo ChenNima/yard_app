169f1b691ea1d573fec570fdb598e2e93331f028
Yifei Chen <yifei.chen@careerbuilder.com>
2016-07-06 11:57:42 +0800

    css

--- www/js/controllers/chat/chat-ctrl.js
+++ www/js/controllers/chat/chat-ctrl.js
@@ -2,18 +2,25 @@
  * Created by CYF on 16/7/5.
  */
 angular.module('starter')
-  .controller('ChatCtrl',function ($scope,Restangular,LocalVariable,socketService,DataFormat) {
+  .controller('ChatCtrl',function ($scope,$ionicLoading,Restangular,LocalVariable,socketService,DataFormat) {
     var user = LocalVariable.getUser();
+
+    var sendData;
 
     $scope.data={};
 
     $scope.data.name = user.nickName;
+
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
 
     socketService.reconnect();
 
     socketService.emit('login',{userName:user.nickName});
 
     socketService.on('chats',function(data){
+      $ionicLoading.hide();
       if ($scope.datas &&  _.last(data)._id!=_.last($scope.datas)._id && _.last(data).name != $scope.data.name){
         //showNotify(_.last(data).name+": "+_.last(data).content);
       }
@@ -30,4 +37,23 @@ angular.module('starter')
     socketService.on('user_login_change',function(data){
       $scope.onlineUsers = data;
     });
+
+    $scope.send = function(){
+      var myDate = new Date();
+      var time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+      $scope.data.date = time;
+      sendData = deepCopy($scope.data);
+      $scope.data.content = "";
+      socketService.emit('add_new',{
+        data : sendData
+      })
+    };
+
+    var deepCopy = function (source) {
+      var result = {};
+      for (var key in source) {
+        result[key] = typeof source[key] === 'object' ? deepCoyp(source[key]) : source[key];
+      }
+      return result;
+    };
   });
