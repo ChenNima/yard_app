8f7c4029a3ac4175f73670ec40d96ca75b5e2fda
Yifei Chen <yifei.chen@careerbuilder.com>
2016-08-11 10:57:21 +0800

    add net

--- www/js/controllers/dash-borad/dash-ctrl.js
+++ www/js/controllers/dash-borad/dash-ctrl.js
@@ -3,25 +3,40 @@
  */
 angular.module('starter')
 
-  .controller('DashCtrl', function ($scope,$location,LocalVariable,NetworkSpeed,$cordovaGeolocation) {
+  .controller('DashCtrl', function ($scope,$location,LocalVariable,NetworkSpeed,$cordovaGeolocation,$cordovaNetwork,$ionicPlatform) {
 
     $scope.signal =0;
     $scope.lat =0;
     $scope.long =0;
+    $scope.type =0;
 
 
     var posOptions = {timeout: 10000, enableHighAccuracy: true};
-    $cordovaGeolocation
-      .getCurrentPosition(posOptions)
-      .then(function (position) {
-        $scope.lat  = position.coords.latitude
-        $scope.long = position.coords.longitude
-      }, function(err) {
-        // error
-      });
 
     NetworkSpeed.getSpeed(function(data){
       $scope.signal = data;
+    });
+
+    $ionicPlatform.ready(function() {
+
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+          $scope.lat  = position.coords.latitude
+          $scope.long = position.coords.longitude
+        }, function(err) {
+          // error
+        });
+
+      try{
+        $scope.netType = $cordovaNetwork.getNetwork();
+      }catch(e) {
+        console.log(e);
+      }
+
+      //var isOnline = $cordovaNetwork.isOnline();
+      //
+      //var isOffline = $cordovaNetwork.isOffline();
     });
 
     $scope.logout = function (){
