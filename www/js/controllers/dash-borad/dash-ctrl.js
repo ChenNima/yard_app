71005de6c5fe7f1ed7e10318395a920ee191bfe1
Yifei Chen <yifei.chen@careerbuilder.com>
2016-08-11 11:19:14 +0800

    add net

--- www/js/controllers/dash-borad/dash-ctrl.js
+++ www/js/controllers/dash-borad/dash-ctrl.js
@@ -3,12 +3,15 @@
  */
 angular.module('starter')
 
-  .controller('DashCtrl', function ($scope,$location,LocalVariable,NetworkSpeed,$cordovaGeolocation,$cordovaNetwork,$ionicPlatform) {
+  .controller('DashCtrl', function ($scope,$location,LocalVariable,NetworkSpeed,$cordovaGeolocation,$cordovaNetwork,$ionicPlatform,$cordovaDeviceMotion) {
 
     $scope.signal =0;
     $scope.lat =0;
     $scope.long =0;
     $scope.type =0;
+    $scope.X =0;
+    $scope.Y =0;
+    $scope.Z =0;
 
 
     var posOptions = {timeout: 10000, enableHighAccuracy: true};
@@ -33,6 +36,15 @@ angular.module('starter')
       }catch(e) {
         console.log(e);
       }
+
+      $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+        $scope.X = result.x;
+        $scope.Y = result.y;
+        $scope.Z = result.z;
+        var timeStamp = result.timestamp;
+      }, function(err) {
+        // An error occurred. Show a message to the user
+      });
 
       //var isOnline = $cordovaNetwork.isOnline();
       //
